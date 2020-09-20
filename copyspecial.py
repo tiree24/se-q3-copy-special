c4fa264916d84e418461c6f2f0b780fee47717aa
Tiree Jackson <tiree_jackson@yahoo.com>
2020-09-20 15:03:43 -0400

    finsih the assessment

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "tiree Daniel help me figure out solving the problem."
 
 import re
 import os
@@ -20,17 +20,29 @@ import argparse
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
     # your code here
-    return
+    list = []
+    paths = os.listdir(dirname)
+    for filename in paths:
+        match = re.search(r'__(\w+)__', filename)
+        if match:
+            list.append(os.path.abspath(os.path.join(dirname, filename)))
+    return list
 
 
 def copy_to(path_list, dest_dir):
-    # your code here
-    return
+    """copy the special files to a directory"""
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    for path in path_list:
+        filename = os.path.basename(path)
+        shutil.copy(path, os.path.join(dest_dir, filename))
 
 
 def zip_to(path_list, dest_zip):
-    # your code here
-    return
+    """zips special files"""
+    cmd = ['zip', '-j', dest_zip]
+    cmd += path_list
+    subprocess.run(cmd)
 
 
 def main(args):
@@ -39,17 +51,28 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('from_dir', help='dir to search')
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
+    # print(ns.from_dir)
+    # print(ns.todir)
+    # print(ns.tozip)
+
+    special_paths = get_special_paths(ns.from_dir)
+
+    if ns.todir:
+        copy_to(special_paths, ns.todir)
+    elif ns.tozip:
+        zip_to(special_paths, ns.tozip)
+    else:
+        print('\n'.join(special_paths))
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
-
     # Parsing command line arguments is a must-have skill.
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
-
     # Your code here: Invoke (call) your functions
 
 
